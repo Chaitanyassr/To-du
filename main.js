49c7ed7c76a39f4ea2a5cd17a791c7b6728ccb86
Chaitanya <chaitanyas0101@gmail.com>
2020-03-01 22:49:31 +0530

    added js code

---
+++ main.js
@@ -0,0 +1,7 @@
+function todo(){
+	var item  = document.getElementById('todoinput').value
+	var text = document.createTextNode(item)
+	var newItem = document.createElement("li")
+	newItem.appendChild(text)
+	document.getElementById("todolist").appendChild(newItem)
+}
